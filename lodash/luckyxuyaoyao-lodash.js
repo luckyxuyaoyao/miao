c8c79e078bf61ffb25f82d2c923a981194ef2887
luckyxuyaoyao <xuyaoyaolucky@163.com>
2020-04-28 17:00:29 +0800

    y

---
+++ lodash/luckyxuyaoyao-lodash.js
@@ -0,0 +1,24 @@
+<script src="lodash.js"></script>
+<script>
+    var luckyxuyaoyao = {
+        
+        isNull:function(val){
+            if(val == null){
+                return true
+            }else{
+                return false
+            }
+        },
+        
+        isNaN:function(val){
+            if(val !== val){
+                return true
+            }else{
+                return false
+            }
+        },
+
+
+    }
+
+</script>
